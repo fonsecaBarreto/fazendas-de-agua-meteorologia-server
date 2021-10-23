de2fd5ef306ca67d91f03e24ed2778f40d627e33
fonsecaBarreto <lucasbfonte@gmail.com>
2021-10-23 17:50:20 -0300

    multiples measurements controller refactored

--- src/presentation/Controllers/V1/Admin/Measurements.Controller.ts
+++ src/presentation/Controllers/V1/Admin/Measurements.Controller.ts
@@ -1,42 +1,29 @@
 import { AccessType, BaseController, Ok, NotFound, Request, Response, BadRequest, Unprocessable  } from "../../../Protocols/BaseController";
-import { SchemaValidator } from "../../../../libs/ApplicatonSchema/SchemaValidator";
 import { CsvReader, Errors } from "../../../../libs/CsvReader";
-import { Measurement_CreateBodySchema, MutiplesMeasurements_CreateParamsSchema } from '../../../Models/Schemas/MeaserumentsSchemas'
+import { MutiplesMeasurements_CreateParamsSchema } from '../../../Models/Schemas/MeaserumentsSchemas'
 import { IMeasurementsService } from "../../../../domain/Services/Stations/Measurements_Services";
-import { PgStationsRepository } from "../../../../infra/db";
 import { StationNotFoundError } from "../../../../domain/Errors/StationsErrors";
 import { IStationRepository } from "../../../../domain/Interfaces";
-import { parseCommandLine } from "typescript";
+import { MultiplesMeasurementsValidator } from "../Helpers/MultiplesMeasurementsValidator";
 
-export type CsvConflict = Record<string, SchemaValidator.Errors>
 
 export class CreateMultiplesMeasurementsController extends BaseController {
      constructor(
           private readonly csvReader: CsvReader,
-          private readonly validator: SchemaValidator,
+          private readonly measurementsValidator: MultiplesMeasurementsValidator,
           private readonly measurementsServices: Pick<IMeasurementsService,'create'>,
-          private readonly stationRepository: Pick<IStationRepository,'find'>
+          private readonly stationRepository: Pick<IStationRepository,'find'>,
          
      ){ super( AccessType.ADMIN, {
           params: MutiplesMeasurements_CreateParamsSchema
      })}
 
-     async entryValidator(list:any[]): Promise<CsvConflict> {
-
-          const conflits: CsvConflict = {}
-          await Promise.all(list.map(async (entry, index) =>{
-               const errors = await this.validator.validate(Measurement_CreateBodySchema, entry);
-               if(errors){
-                    conflits[index] = errors
-               }
-          }))
-
-          return conflits
-     }
-
      async handler(request: Request): Promise<Response> {
 
+          const f = request.query.f === "1" ? true : false
+
           const station_id = request.params.station_id;
+
           const stationExists = await this.stationRepository.find(station_id)
           if(!stationExists) return NotFound(new StationNotFoundError())
 
@@ -47,21 +34,22 @@ export class CreateMultiplesMeasurementsController extends BaseController {
 
                const measurementsList = await this.csvReader.read(csv_entry[0].buffer)
 
-               const errors = await this.entryValidator(measurementsList)
+               const errors = await this.measurementsValidator.execute(measurementsList, station_id, f)
 
                if(Object.keys(errors).length > 0)
                     return Unprocessable(errors, "O Arquivo .Csv Contem dados insatisfatórios");
 
-               await Promise.all(measurementsList.map( async ( createParams: IMeasurementsService.Params.Create)=>{
-                    await this.measurementsServices.create({ ...createParams, station_id })
+               const stored = await Promise.all(measurementsList.map( async ( createParams: IMeasurementsService.Params.Create, i)=>{
+                    const result = await this.measurementsServices.create({ ...createParams, station_id })
+                    return result
                }))
-
-               return Ok();
+ 
+               return Ok(stored);
                
           } catch (err) {
-               if(err instanceof Errors.InvalidCsvFile){
-                    return BadRequest(err.message)
-               }
+          
+               if(err instanceof Errors.InvalidCsvFile )
+                    return BadRequest(err.message);
                throw err
           }
      }
