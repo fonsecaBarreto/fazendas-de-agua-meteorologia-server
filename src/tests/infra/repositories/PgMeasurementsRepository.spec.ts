a245d906c395d5aca09e77c60565367ebdfa103d
fonsecaBarreto <lucasbfonte@gmail.com>
2021-10-23 16:47:07 -0300

    udpate pg Measurements Repository tests

--- src/tests/infra/repositories/PgMeasurementsRepository.spec.ts
+++ src/tests/infra/repositories/PgMeasurementsRepository.spec.ts
@@ -11,8 +11,9 @@ import { Knex } from 'knex'
 const fakedAddresses = [ MakeFakeAddress({}) ];
 const fakedStations = [ MakeFakeStation({ address_id: fakedAddresses[0].id}) ]
 const fakedMeasurements = [  
-     MakeFakeMeasurement({ station_id: fakedStations[0].id, created_at: new Date("2014-05-22") }), 
-     MakeFakeMeasurement({ station_id: fakedStations[0].id, created_at: new Date("2024-05-22") }) 
+     MakeFakeMeasurement({ station_id: fakedStations[0].id, created_at: new Date("2022-04-22") }), 
+     MakeFakeMeasurement({ station_id: fakedStations[0].id, created_at: new Date("2022-05-22") }) ,
+     MakeFakeMeasurement({ station_id: fakedStations[0].id, created_at: new Date("2022-06-22") }) 
 ]
 
 const makeSut = () =>{
@@ -29,8 +30,6 @@ describe("Measurements Pg Repository", () =>{
           await KnexAdapter.connection("measurements").insert(fakedMeasurements)
      })
      afterAll(async ()=>{ await KnexAdapter.close()})
-
-
 
      describe("Find Measurement by station_id and Date", () => {
           test("should return null if no measurement were found", async () =>{
@@ -60,23 +59,35 @@ describe("Measurements Pg Repository", () =>{
 
      describe("Should add measurement", () => {
           test("should return null if no measurement were found", async () =>{
-
                const  count  = await KnexAdapter.count('measurements')
-
                const sut = makeSut();
                await sut.add(MakeFakeMeasurement({station_id: fakedStations[0].id, created_at: new Date('2040-10-02')}));
-           
                const recontagem = await KnexAdapter.count('measurements')
                expect(recontagem).toBe( count + 1 )
           })
-          test(" Should not overwrite 'created_at' param", async () =>{
 
+          test(" Should create data", async () =>{
                const sut = makeSut();
-               const fakeData = MakeFakeMeasurement({station_id: fakedStations[0].id, created_at: new Date()})
+               const fakeData = MakeFakeMeasurement({station_id: fakedStations[0].id, created_at: new Date('2020-10-02')})
                await sut.add( fakeData );
-              
                const data = await KnexAdapter.connection('measurements').where({ id: fakeData.id }).first()
                expect(data).toEqual(fakeData)
+          }) 
+
+
+          test(" Should merge ( measurements data only ) if same date to the same station where provided ", async () =>{
+               
+               const sut = makeSut();
+               const fakeData = MakeFakeMeasurement({ station_id: fakedMeasurements[2].station_id, created_at: fakedMeasurements[2].created_at})
+
+               await sut.add(fakeData);
+
+               var data = await KnexAdapter.connection('measurements').where({ id: fakeData.id }).first()
+               expect(data).toBeFalsy()
+
+               data = await KnexAdapter.connection('measurements').where({ id: fakedMeasurements[2].id }).first()
+               expect(data).toEqual({ ...fakeData, id: fakedMeasurements[2].id})
+      
           }) 
      })
 
