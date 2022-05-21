90fda252229df65ac001755cf1e8563306715f7c
fonsecaBarreto <lucasbfonte@gmail.com>
2022-05-21 19:47:53 -0300

    new seed

--- db/test/test_general.js
+++ db/test/test_general.js
@@ -12,18 +12,18 @@ const createUser = (role, username, name) =>({
 
 const createAddress = () =>({
      id: v4(),
-     street: "Rua teste",
-     region:"Bairro Qualquer",
+     street: "R. Aloísio da Silva Gomes",
+     region:"Granja dos Cavaleiros",
      uf: "RJ",
-     number:'Cassa 1',
-     city: "Rio das Ostras",
+     number:'50',
+     city: "Macaé",
      details: "Detalhe teste",
-     postalCode: "00000000"
+     postalCode: "27930560"
 })
 
 const createStation = (address_id) =>({
      id: v4(),
-     description: "estação teste",
+     description: "Estação A1",
      longitude: 213,
      latitude: 123,
      altitude: 123,
@@ -32,11 +32,11 @@ const createStation = (address_id) =>({
 
 
 function createJSON(){
-     const addresses = [ createAddress(), ...([...Array(2)].map(()=>createAddress()))];
+     const addresses = [ createAddress(), ...([...Array(6)].map(()=>createAddress()))];
      const stations=  [ createStation(addresses[0].id) ];
      const admins = [ createUser(1, "admin", "Administrador Super") ];
      const users= [ createUser(0, 'user_basic', "Usuario Basico"), ...([...Array(2)].map((j,i)=>createUser(0, `user_basic_0${i}`, `Usuario Basico ${i}`)))]
-     const measurements = generateStationMeasurements(stations[0].id, 30 * 3 )//30 * 3) 
+     const measurements = generateStationMeasurements(stations[0].id, 30  )//30 * 3) 
 
      const payload = {
           addresses,
